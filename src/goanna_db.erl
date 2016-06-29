74edb66ce015cda5b976e342667ff8e4aa08b9aa
ruanpienaar <ruan800@gmail.com>
2016-06-29 16:28:43 +0100

    WIP #3 remove traces working.

--- src/goanna_db.erl
+++ src/goanna_db.erl
@@ -4,7 +4,9 @@
           init_node/1,
           store/2,
           lookup/1,
-          terminate_node/1
+          terminate_node/1,
+          delete_tracelist_pattern/2,
+          truncate_tracelist/1
 ]).
 
 %% API
@@ -18,12 +20,17 @@ init_node([Node, Cookie]) ->
     Name = goanna_sup:id(Node, Cookie),
     ets:new(Name, [public, ordered_set, named_table]).
 
-store([trace_pattern, Node, Cookie], Trace) ->
+store([trace_pattern, Node, Cookie], TracePattern) ->
     case ets:lookup(tracelist, {Node, Cookie}) of
         [{{Node, Cookie}, TracePatterns}] ->
-            ets:insert(tracelist, {{Node, Cookie},[Trace|TracePatterns]});
+            case lists:member(TracePattern, TracePatterns) of
+                true ->
+                    {error, already_traced};
+                false ->
+                    ets:insert(tracelist, {{Node, Cookie},[TracePattern|TracePatterns]})
+            end;
         [] ->
-            ets:insert(tracelist, {{Node, Cookie},[Trace]})
+            ets:insert(tracelist, {{Node, Cookie},[TracePattern]})
     end;
 store([trace, Node, Cookie], Trace) ->
     Name = goanna_sup:id(Node, Cookie),
@@ -34,3 +41,15 @@ lookup([trc_pattern, Node, Cookie]) ->
 
 terminate_node([Node, Cookie]) ->
     ets:delete(nodelist, {Node,Cookie}).
+
+delete_tracelist_pattern([Node, Cookie], TracePattern) ->
+    case lookup([trc_pattern, Node, Cookie]) of
+        [] ->
+            [];
+        [{{Node, Cookie},TracePatterns}] ->
+            Updated=lists:delete(TracePattern, TracePatterns),
+            ets:insert(tracelist, {{Node, Cookie},Updated})
+    end.
+
+truncate_tracelist([Node, Cookie]) ->
+    ets:delete(tracelist, {Node, Cookie}).
