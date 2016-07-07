668b4c16af653147a5d3c10daaffb5e3f5af72aa
ruanpienaar <ruan800.com>
2016-07-07 15:12:49 +0100

    ....

--- start-dev.sh
+++ start-dev.sh
@@ -3,4 +3,4 @@
 # -mnesia dir "'"$PWD"/Mnesia'"
 rebar compile
 cd `dirname $0`
-exec ./deps/kjell/bin/kjell -sname goanna -config $PWD/sys.config -pa $PWD/ebin $PWD/deps/*/ebin $PWD/test -cookie goanna -s goanna_api start
+exec erl -sname goanna -config $PWD/sys.config -pa $PWD/ebin $PWD/deps/*/ebin $PWD/test -cookie goanna -s goanna_api start
