46429a5af9bc2aae34592eef615cb0f860dd5337
ruanpienaar <ruan800@gmail.com>
2016-09-10 15:49:16 +0100

    start script

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # -mnesia dir "'"$PWD"/Mnesia'"
-rebar clean app=goanna
-rebar compile
+#./rebar clean app=goanna
+#./rebar compile
 cd `dirname $0`
 erl +A 1 +K true -sname goanna -config $PWD/sys.config -pa $PWD/ebin $PWD/deps/*/ebin $PWD/test -setcookie goanna -s goanna_api start -hidden #-noshell -noinput -detached
