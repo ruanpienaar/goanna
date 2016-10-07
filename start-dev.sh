39475a378b9f0b121770e7f5afe969d4220d2c82
ruanpienaar <ruan800@gmail.com>
2016-10-07 11:45:50 +0100

    merge freakin conflict

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 # -mnesia dir "'"$PWD"/Mnesia'"
+
+if [ -z "$1" ]; then
+    DIST_NAME="-name"
+else
+    DIST_NAME="$1"
+fi
+
+if [ -z "$2" ]; then
+    NAME="goanna"
+else
+    NAME="$2"
+fi
+
+#Dev steps:
 #./rebar clean app=goanna
 #./rebar compile
+
 cd `dirname $0`
-erl +A 1 +K true -name goanna -config $PWD/sys.config -pa $PWD/ebin $PWD/deps/*/ebin $PWD/test -setcookie goanna -s goanna_api start -hidden #-noshell -noinput -detached
+erl +A 1 +K true $DIST_NAME $NAME -config $PWD/sys.config -pa $PWD/ebin $PWD/deps/*/ebin $PWD/test -setcookie goanna -s goanna_api start -hidden #-noshell -noinput -detached
+>>>>>>> start script updated
