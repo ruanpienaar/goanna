5135e49b40e1c6d8d6535059d53a9c5dd0130049
ruanpienaar <ruan800@gmail.com>
2017-05-13 13:24:21 +0100

    ..

--- tests.sh
+++ tests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# Original
-# rebar compile && rebar skip_deps=true eunit && rebar skip_deps=true ct
-rebar3 clean 
-rebar3 compile 
+rebar3 clean
+rebar3 compile
 rebar3 eunit skip_deps=true
